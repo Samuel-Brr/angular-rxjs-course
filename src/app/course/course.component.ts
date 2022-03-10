a561894a2ac2616f96ec30cbea55f4d10ffa8b47
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-10 14:05:07 +0100

    The startWith RxJs Operator - Simplifying the Course Component

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -52,7 +52,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
         .pipe(
           map(event => event.target.value),
           startWith(""),
-          debounceTime(400),
+          debounceTime(401),
           distinctUntilChanged(),
           switchMap(search => this.loadLessons(search))
 
