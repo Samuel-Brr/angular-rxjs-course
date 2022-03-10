27a7c6ce13f6d5baff18d00e187ed17ed7bfb3ae
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-10 15:30:51 +0100

    The RxJs Debug Operator - Implementation Conclusion

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -16,7 +16,7 @@ import {
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
-import { debug } from '../common/debug';
+import { debug, RxJsLoggingLevel } from '../common/debug';
 
 
 @Component({
@@ -43,6 +43,9 @@ export class CourseComponent implements OnInit, AfterViewInit {
         this.courseId = this.route.snapshot.params['id'];
 
         this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+          .pipe(
+            debug(RxJsLoggingLevel.INFO, "course value")
+          )
 
 
     }
@@ -53,10 +56,11 @@ export class CourseComponent implements OnInit, AfterViewInit {
         .pipe(
           map(event => event.target.value),
           startWith(""),
-          debug(RxJsLoggingLevel.INFO, "search"),
+          debug( RxJsLoggingLevel.INFO, "search"),
           debounceTime(401),
           distinctUntilChanged(),
-          switchMap(search => this.loadLessons(search))
+          switchMap(search => this.loadLessons(search)),
+          debug(RxJsLoggingLevel.INFO, "lesson value")
 
         )
 
